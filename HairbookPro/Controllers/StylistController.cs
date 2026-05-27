d67ad99224e6342d07e58b3ccee1c9a21aa7020a
Ivana <kivanak04@gmail.com>
2026-05-27 18:55:24 +0200

    add stylist reviews

--- HairbookPro/Controllers/StylistController.cs
+++ HairbookPro/Controllers/StylistController.cs
@@ -1,6 +1,10 @@
 ﻿using Hairbookpro.Models;
 using Hairbookpro.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -17,7 +21,48 @@ namespace Hairbookpro.Controllers
                 .OrderBy(s => s.FullName)
                 .ToList();
 
+            var ratings = new Dictionary<int, decimal>();
+
+            foreach (var stylist in stylists)
+            {
+                var rating = db.Database.SqlQuery<decimal>(
+                    "SELECT dbo.fn_GetAverageStylistRating(@StylistId)",
+                    new SqlParameter("@StylistId", stylist.Id)
+                ).FirstOrDefault();
+
+                ratings[stylist.Id] = rating;
+            }
+
+            ViewBag.Ratings = ratings;
+
             return View(stylists);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddReview(int stylistId, int rating, string comment)
+        {
+            if (rating < 1 || rating > 5)
+            {
+                TempData["ReviewError"] = "Ocjena mora biti od 1 do 5.";
+                return RedirectToAction("Index");
+            }
+
+            var review = new Review
+            {
+                StylistId = stylistId,
+                UserId = User.Identity.GetUserId(),
+                Rating = rating,
+                Comment = comment,
+                CreatedAt = DateTime.Now
+            };
+
+            db.Reviews.Add(review);
+            db.SaveChanges();
+
+            TempData["ReviewSuccess"] = "Ocjena je uspješno dodata.";
+            return RedirectToAction("Index");
         }
 
         public ActionResult Details(int id)
