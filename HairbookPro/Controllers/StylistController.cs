b82a7fc8f389acae228c6bb7e573ae40ef7677bd
Ivana <kivanak04@gmail.com>
2026-05-27 16:59:18 +0200

    add missing stylist files

---
+++ HairbookPro/Controllers/StylistController.cs
@@ -0,0 +1,37 @@
+﻿using Hairbookpro.Models;
+using HairBookPro.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace HairBookPro.Controllers
+{
+    public class StylistsController : Controller
+    {
+        private readonly ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Index()
+        {
+            var stylists = db.Stylists
+                .Where(s => s.IsActive)
+                .OrderBy(s => s.FullName)
+                .ToList();
+
+            return View(stylists);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var stylist = db.Stylists
+                .Include(s => s.Reviews)
+                .FirstOrDefault(s => s.Id == id);
+
+            if (stylist == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(stylist);
+        }
+    }
+}
