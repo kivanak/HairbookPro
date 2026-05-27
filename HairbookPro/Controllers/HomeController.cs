8f8cf5e5d5cae126d93a7ae263ec8152091b7eac
Ivana <kivanak04@gmail.com>
2026-05-27 16:51:56 +0200

    style polish + encoding problem fixed

--- HairbookPro/Controllers/HomeController.cs
+++ HairbookPro/Controllers/HomeController.cs
@@ -3,14 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using System.Linq;
+using HairBookPro.Models;
+using Hairbookpro.Models;
 
 namespace Hairbookpro.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
-            return View();
+            var model = new HomeViewModel
+            {
+                Services = db.Services
+                    .Where(s => s.IsActive)
+                    .OrderBy(s => s.Name)
+                    .Take(4)
+                    .ToList(),
+
+                Stylists = db.Stylists
+                    .Where(s => s.IsActive)
+                    .OrderBy(s => s.FullName)
+                    .Take(3)
+                    .ToList(),
+
+                BlogPosts = db.BlogPosts
+                    .Include(b => b.BlogCategory)
+                    .OrderByDescending(b => b.CreatedAt)
+                    .Take(2)
+                    .ToList()
+            };
+
+            ViewBag.ServicesDropdown = db.Services
+                .Where(s => s.IsActive)
+                .OrderBy(s => s.Name)
+                .ToList();
+
+            return View(model);
         }
 
         public ActionResult About()
---
+++ HairbookPro/Models/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Hairbookpro.Models;
+using System.Collections.Generic;
+
+namespace HairBookPro.Models
+{
+    public class HomeViewModel
+    {
+        public List<Service> Services { get; set; }
+
+        public List<Stylist> Stylists { get; set; }
+
+        public List<BlogPost> BlogPosts { get; set; }
+    }
+}
