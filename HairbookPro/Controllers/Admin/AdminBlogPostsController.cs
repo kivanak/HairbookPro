5356aea2d5d309a2973fdc10e0c6edc6fd4add6c
Ivana <kivanak04@gmail.com>
2026-05-27 18:17:26 +0200

    add admin blog post managing

--- HairbookPro/Controllers/Admin/AdminBlogPostsController.cs
+++ HairbookPro/Controllers/Admin/AdminBlogPostsController.cs
@@ -5,38 +5,133 @@ using System.Net;
 using System.Web.Mvc;
 using Hairbookpro.Models;
 
-namespace Hairbookpro.Controllers.Admin
+namespace Hairbookpro.Controllers
 {
     [Authorize(Roles = "Admin")]
     public class AdminBlogPostsController : Controller
     {
         private readonly ApplicationDbContext db = new ApplicationDbContext();
 
+        // GET: AdminBlogPosts
         public ActionResult Index()
         {
-            return View(db.BlogPosts.Include(p => p.BlogCategory).OrderByDescending(p => p.CreatedAt).ToList());
+            var posts = db.BlogPosts
+                .Include(p => p.BlogCategory)
+                .OrderByDescending(p => p.CreatedAt)
+                .ToList();
+
+            return View(posts);
         }
 
+        // GET: AdminBlogPosts/Create
         public ActionResult Create()
         {
-            ViewBag.BlogCategoryId = new SelectList(db.BlogCategories, "Id", "Name");
+            ViewBag.BlogCategoryId = new SelectList(
+                db.BlogCategories,
+                "Id",
+                "Name"
+            );
+
             return View(new BlogPost());
         }
 
+        // POST: AdminBlogPosts/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(BlogPost post)
         {
+            post.CreatedAt = DateTime.Now;
+
+            ModelState.Remove("BlogCategory");
+            ModelState.Remove("Comments");
+
             if (ModelState.IsValid)
             {
-                post.CreatedAt = DateTime.Now;
                 db.BlogPosts.Add(post);
                 db.SaveChanges();
+
                 return RedirectToAction("Index");
             }
 
-            ViewBag.BlogCategoryId = new SelectList(db.BlogCategories, "Id", "Name", post.BlogCategoryId);
-            return View(post);
+            ViewBag.BlogCategoryId = new SelectList(
+                db.BlogCategories,
+                "Id",
+                "Name",
+                post.BlogCategoryId
+            );
+
+            return View("~/Views/AdminBlogPosts/Create.cshtml", post);
+        }
+
+        // GET: AdminBlogPosts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var post = db.BlogPosts.Find(id);
+
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.BlogCategoryId = new SelectList(
+                db.BlogCategories,
+                "Id",
+                "Name",
+                post.BlogCategoryId
+            );
+
+            return View("~/Views/AdminBlogPosts/Edit.cshtml", post);
+        }
+
+        // POST: AdminBlogPosts/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(BlogPost post)
+        {
+            ModelState.Remove("BlogCategory");
+            ModelState.Remove("Comments");
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(post).State = EntityState.Modified;
+
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.BlogCategoryId = new SelectList(
+                db.BlogCategories,
+                "Id",
+                "Name",
+                post.BlogCategoryId
+            );
+
+            return View("~/Views/AdminBlogPosts/Edit.cshtml", post);
+        }
+
+        // POST: AdminBlogPosts/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            var post = db.BlogPosts.Find(id);
+
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+
+            db.BlogPosts.Remove(post);
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
